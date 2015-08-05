8f14a94a0002f2549353118af3812222332fba26
Edan Schwartz <edan@edanschwartz.com>
2015-08-05 17:24:09 -0500

    Setup Alice fixture loading in FuncTestCase

--- tests/ZfAuthTest/Functional/FunctionalTestCase.php
+++ tests/ZfAuthTest/Functional/FunctionalTestCase.php
@@ -3,6 +3,8 @@
 namespace Aeris\ZfAuthTest\Functional;
 
 use Aeris\ZendRestModuleTest\AbstractTestCase;
+use Doctrine\ORM\EntityManagerInterface;
+use Nelmio\Alice;
 
 
 class FunctionalTestCase extends AbstractTestCase {
@@ -11,10 +13,15 @@ class FunctionalTestCase extends AbstractTestCase {
 		$this->appConfigDir = __DIR__ . '/../../config';
 	}
 
+	protected function setUp() {
+		parent::setUp();
+		$this->purgeDb();
+	}
+
 	protected function requestAccessToken(array $requestParams = []) {
 		$requestParams = array_replace([
 			'client_id' => 'testclient',
-			'client_secret' => 'testpass',
+			'client_secret' => 'testclient',
 			'username' => 'test_username',
 			'password' => 'test_password',
 			'grant_type' => 'password'
@@ -32,5 +39,31 @@ class FunctionalTestCase extends AbstractTestCase {
 			throw new \Exception('Failed to get access code: ' . $error->getMessage());
 		}
 		return $accessToken;
+	}
+
+	protected function purgeDb() {
+		/** @var EntityManagerInterface $entityManager */
+		$entityManager = $this->getEntityManager();
+		$sm = $entityManager->getConnection()->getSchemaManager();
+		$tables = $sm->listTableNames();
+
+		$entityManager->getConnection()->exec('SET foreign_key_checks = 0');
+		foreach ($tables as $name) {
+			$entityManager->getConnection()->executeUpdate("TRUNCATE $name");
+		}
+		$entityManager->getConnection()->exec('SET foreign_key_checks = 1');
+	}
+
+	protected function loadFixtures($path) {
+		$fixtures = Alice\Fixtures::load($path, $this->getEntityManager());
+		$this->getEntityManager()->flush();
+
+		return $fixtures;
+	}
+
+	/** @return EntityManagerInterface */
+	protected function getEntityManager() {
+		return $this->getApplicationServiceLocator()
+			->get('doctrine.entitymanager.orm_default');
 	}
 }
