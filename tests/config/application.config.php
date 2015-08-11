91f23a0d1a70d7c12c3db224cc31d20a3559d28f
Edan Schwartz <eschwartz@weathernation.net>
2015-08-11 10:03:55 -0500

    test boostrapping config

--- tests/config/application.config.php
+++ tests/config/application.config.php
@@ -6,8 +6,9 @@ return [
 		'DoctrineORMModule',
 		'ZF\OAuth2',
 		'ZF\ContentNegotiation',
+		'Aeris\ZendRestModule',
 		'Aeris\ZfAuth',
-		'Aeris\ZendRestModule'
+		'Aeris\ZfDiConfig'
 	],
 
 	// These are various options for the listeners attached to the ModuleManager
@@ -28,43 +29,5 @@ return [
 		'config_glob_paths' => [
 			'config/autoload/{,*.}{global,local}.php',
 		],
-
-		// Whether or not to enable a configuration cache.
-		// If enabled, the merged configuration will be cached and used in
-		// subsequent requests.
-		//'config_cache_enabled' => $booleanValue,
-
-		// The key used to create the configuration cache file name.
-		//'config_cache_key' => $stringKey,
-
-		// Whether or not to enable a module class map cache.
-		// If enabled, creates a module class map cache which will be used
-		// by in future requests, to reduce the autoloading process.
-		//'module_map_cache_enabled' => $booleanValue,
-
-		// The key used to create the class map cache file name.
-		//'module_map_cache_key' => $stringKey,
-
-		// The path in which to cache merged configuration.
-		//'cache_dir' => $stringPath,
-
-		// Whether or not to enable modules dependency checking.
-		// Enabled by default, prevents usage of modules that depend on other modules
-		// that weren't loaded.
-		// 'check_dependencies' => true,
-	],
-
-	// Used to create an own service manager. May contain one or more child arrays.
-	//'service_listener_options' => array(
-	//     array(
-	//         'service_manager' => $stringServiceManagerName,
-	//         'config_key'      => $stringConfigKey,
-	//         'interface'       => $stringOptionalInterface,
-	//         'method'          => $stringRequiredMethodName,
-	//     ),
-	// )
-
-	// Initial configuration with which to seed the ServiceManager.
-	// Should be compatible with Zend\ServiceManager\Config.
-	// 'service_manager' => array(),
+	]
 ];
