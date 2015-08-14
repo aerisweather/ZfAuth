68ddb74e0070ca98cd17f740ca84faf4364ce120
Edan Schwartz <eschwartz@aerisweather.net>
2015-08-14 12:28:02 -0500

    AggregateRouteGuard

---
+++ src/ZfAuth/Guard/AggregateRouteGuard.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace Aeris\ZfAuth\Guard;
+
+
+use Zend\Mvc\Router\RouteMatch;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Aeris\Fn;
+
+class AggregateRouteGuard implements GuardInterface {
+
+	/** @var ServiceLocatorInterface */
+	protected $routeGuardManager;
+
+	/** @var GuardInterface[] */
+	protected $guards = [];
+
+	public function __construct(array $rules = []) {
+		if (count($rules)) {
+			throw new \InvalidArgumentException('Unable to set rules on a AggregateRouteGuard until a RouteGuardManager is set.');
+		}
+	}
+
+	public function setRules(array $rules) {
+		$this->guards = [];
+
+		foreach ($rules as $guardName => $guardRules) {
+			/** @var GuardInterface $guard */
+			$guard = $this->routeGuardManager->get($guardName);
+			$guard->setRules($guardRules);
+
+			$this->guards[] = $guard;
+		}
+	}
+
+	/** @return boolean */
+	public function isGranted(RouteMatch $routeMatch) {
+		// No guards --> not granted by default
+		if (!count($this->guards)) {
+			return false;
+		}
+
+		return Fn\all($this->guards, function(GuardInterface $guard) use ($routeMatch) {
+			return $guard->isGranted($routeMatch);
+		});
+	}
+
+	/**
+	 * @param ServiceLocatorInterface $routeGuardManager
+	 */
+	public function setRouteGuardManager(ServiceLocatorInterface $routeGuardManager) {
+		$this->routeGuardManager = $routeGuardManager;
+	}
+}
---
+++ tests/ZfAuthTest/Guard/AggregateRouteGuardTest.php
@@ -0,0 +1,117 @@
+<?php
+
+
+namespace Aeris\ZfAuthTest\Guard;
+
+
+use Aeris\ZfAuth\Guard\AggregateRouteGuard;
+use Aeris\ZfAuthTest\Fixture\ServiceManager\ServiceLocator;
+use Zend\Mvc\Router\Http\RouteMatch;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Mockery as M;
+
+class AggregateRouteGuardTest extends \PHPUnit_Framework_TestCase {
+
+	/** @var ServiceLocatorInterface */
+	protected $routeGuardManager;
+
+	protected function setUp() {
+		parent::setUp();
+
+		$this->routeGuardManager = new ServiceLocator();
+		$this->routeGuardManager->set('AlwaysGrantedGuard', M::mock('\Aeris\ZfAuth\Guard\GuardInterface', [
+			'isGranted' => true,
+			'setRules' => null,
+		]));
+		$this->routeGuardManager->set('NeverGrantedGuard', M::mock('\Aeris\ZfAuth\Guard\GuardInterface', [
+			'isGranted' => false,
+			'setRules' => null
+		]));
+	}
+
+	/** @test */
+	public function isGranted_shouldSetRulesOnGuards() {
+		$guard = new AggregateRouteGuard();
+		$guard->setRouteGuardManager($this->routeGuardManager);
+		$guard->setRules([
+			'AlwaysGrantedGuard' => [
+				'foo' => 'bar'
+			],
+			'NeverGrantedGuard' => [
+				'faz' => 'baz'
+			]
+		]);
+
+		$guard->isGranted(new RouteMatch([]));
+
+		$this->routeGuardManager->get('AlwaysGrantedGuard')
+			->shouldHaveReceived('setRules')
+			->once()
+			->with(['foo' => 'bar']);
+
+		$this->routeGuardManager->get('NeverGrantedGuard')
+			->shouldHaveReceived('setRules')
+			->once()
+			->with(['faz' => 'baz']);
+	}
+
+	/** @test */
+	public function isGranted_shouldPassRouteMatchToComponentGuards() {
+		$guard = new AggregateRouteGuard();
+		$guard->setRouteGuardManager($this->routeGuardManager);
+		$guard->setRules([
+			'AlwaysGrantedGuard' => [],
+			'NeverGrantedGuard' => []
+		]);
+
+
+		$routeMatch = new RouteMatch([ 'foo' => 'bar']);
+		$guard->isGranted($routeMatch);
+
+		$isSameRouteMatch = function(RouteMatch $rm) {
+			return $rm->getParam('foo') === 'bar';
+		};
+
+		$this->routeGuardManager->get('AlwaysGrantedGuard')
+			->shouldHaveReceived('isGranted')
+			->once()
+			->with(M::on($isSameRouteMatch));
+
+		$this->routeGuardManager->get('NeverGrantedGuard')
+			->shouldHaveReceived('isGranted')
+			->once()
+			->with(M::on($isSameRouteMatch));
+	}
+
+	/** @test */
+	public function isGranted_shouldFailIfAnyComponentGuardFails() {
+		$guard = new AggregateRouteGuard();
+		$guard->setRouteGuardManager($this->routeGuardManager);
+		$guard->setRules([
+			'AlwaysGrantedGuard' => [],
+			'NeverGrantedGuard' => []
+		]);
+
+		$this->assertFalse($guard->isGranted(new RouteMatch([])));
+	}
+
+	/** @test */
+	public function isGranted_shouldPassIfAllComponentGuardPasses() {
+		$guard = new AggregateRouteGuard();
+		$guard->setRouteGuardManager($this->routeGuardManager);
+		$guard->setRules([
+			'AlwaysGrantedGuard' => [],
+		]);
+
+		$this->assertTrue($guard->isGranted(new RouteMatch([])));
+	}
+
+	/** @test */
+	public function isGranted_shouldFailIfNoRulesAreSet() {
+		$guard = new AggregateRouteGuard();
+		$guard->setRouteGuardManager($this->routeGuardManager);
+
+		$this->assertFalse($guard->isGranted(new RouteMatch([])));
+	}
+
+}
